8748354b0aafa2ab9d6b0766fa7ece7989daae5b
Barclay-McClay <102266976+Barclay-McClay@users.noreply.github.com>
2022-03-24 14:57:29 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1126,6 +1126,109 @@ if (message.substring(0, 1) == '?') { //...If the message starts with '?'...
 				
 	}//CH8 end---
 	
+	//CH 9-----------------------
+	switch(cmd) {
+		
+		case 'CH9.1':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.1** *(G.R.S Mead)*\n>>> I gave the Perfect Sermon (Logos) yesterday, Asclepius; today I think it right, as sequel thereunto, to go through point by point the Sermon about Sense.\nNow sense and thought do seem to differ, in that the former has to do with matter, the latter has to do with substance. But unto me both seem to be at-one and not to differ - in men I mean. In other lives <or living creatures> sense is at-oned with Nature, but in men thought.\nNow mind doth differ just as much from thought as God doth from divinity. For that divinity by God doth come to be, and by mind thought, the sister of the word (logos) and instruments of one another. For neither doth the word (logos) find utterance without thought, nor is thought manifested without word.'				
+			})
+		break;
+		
+		case 'CH9.2':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.2** *(G.R.S Mead)*\n>>> So sense and thought both flow together into man, as though they were entwined with one another. For neither without sensing can one think, nor without thinking sense.\nBut it is possible [they say] to think a thing apart from sense, as those who fancy sights in dreams. But unto me it seems that both of these activities occur in dream-sight, and sense doth pass out of the sleeping to the waking state.\nFor man is separated into soul and body, and only when the two sides of his sense agree together, does utterance of its thought conceived by mind take place.'				
+			})
+		break;
+	
+		case 'CH9.3':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.3** *(G.R.S Mead)*\n>>> For it is mind that doth conceive all thoughts - good thoughts when it receives the seeds from God, their contraries when [it receiveth them] from the daimonials; no part of Cosmos being free of daimon, who stealthily doth creep into the daimon who\'s illumined by God\'s light <i.e., the human soul>, and sow in him the seed of its own energy.\nAnd mind conceives the seed thus sown, adultery, murder, parricide, [and] sacrilege, impiety, [and] strangling, casting down precipices, and all such other deeds as are the work of evil daimons.'
+				
+			})
+		break;
+		
+		case 'CH9.4':
+			bot.sendMessage({
+
+                to: channelID,
+
+               message: '**CH9.4** *(G.R.S Mead)*\n>>> The seeds of God, \'tis true, are few, but vast and fair, and good - virtue and self-control, devotion. Devotion is God-gnosis; and he who knoweth God, being filled with all good things, thinks godly thoughts and not thoughts like the many [think].\nFor this cause they who Gnostic are, please not the many, nor the many them. They are thought mad and laughted at; they\'re hated and despised, and sometimes even put to death.\nFor we did say that bad must needs dwell on earth, where \'tis in its own place. Its place is earth, and not Cosmos, as some will sometimes say with impious tongue.\nBut he who is a devotee of God, will bear with all - once he has sensed the Gnosis. For such an one all things, e\'en though they be for others bad, are for him good; deliberately he doth refer them all unto the Gnosis. And, thing most marvelous, \'tis he alone who maketh bad things good.'
+
+			   })
+		break;
+		
+		case 'CH9.5':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.5** *(G.R.S Mead)*\n>>> But I return once more to the Discourse (Logos) on Sense. That sense doth share with thought in man, doth constitute him man. But \'tis not [every] man, as I have said, who benefits by thought; for this man is material, that other one substantial.\nFor the material man, as I have said, [consorting] with the bad, doth have his seed of thought from daimons; while the substantial men [consorting] with the Good, are saved by God.\nNow God is Maker of all things, and in His making, He maketh all [at last] like to Himself; but they, while they\'re becoming good by exercise of their activity, are unproductive things.\nIt is the working of the Cosmic Course that maketh their becomings what they are, befouling some of them with bad and others of them making clean with good.\nFor Cosmos, too, Asclepius, possesseth sense-and-thought peculiar to itself, not like that of man; \'tis not so manifold, but as it were a better and a simpler one.'			  
+			 })
+		break;
+		
+		case 'CH9.6':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.6** *(G.R.S Mead)*\n>>> The single sense-and-thought of Cosmos is to make all things, and make them back into itself again, as Organ of the Will of God, so organized that it, receiving all the seeds into itself from God, and keeping them within itself, may make all manifest, and [then] dissolving them, make them all new again; and thus, like a Good Gardener of Life, things that have been dissolved, it taketh to itself, and giveth them renewal once again.\nThere is no thing to which it gives not life; but taking all unto itself it makes them live, and is at the same time the Place of Life and its Creator.'				
+
+				})
+		break;
+		
+		case 'CH9.7':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.7** *(G.R.S Mead)*\n>>> Now bodies matter [-made] are in diversity. Some are of earth, of water some, some are of air, and some of fire.\nBut they are all composed; some are more [composite], and some are simpler. The heavier ones are more [composed], the lighter less so.\nIt is the speed of Cosmos\' Course that works the manifoldness of the kinds of births. For being a most swift Breath, it doth bestow their qualities on bodies together with the One Pleroma - that of Life.'
+
+				})
+		break;
+		
+		case 'CH9.8':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.8** *(G.R.S Mead)*\n>>> God, then, is Sire of Cosmos; Cosmos, of all in Cosmos. And Cosmos is God\'s Son; but things in Cosmos are by Cosmos.\nAnd properly hath it been called Cosmos [Order]; for that it orders all with their diversity of birth, with its not leaving aught without its life, with the unweariedness of its activity, the speed of its necessity, the composition of its elements, and order of its creatures.\nThe same, then, of necessity and propriety should have the name of Order.\nThe sense-and-thought, then, of all lives doth come into them from without, inbreathed by what contains [them all]; whereas Cosmos receives them once for all together with its coming into being, and keeps them as a gift from God.'
+
+				})
+		break;
+		
+		case 'CH9.9':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.9** *(G.R.S Mead)*\n>>> But God is not, as some suppose, beyond the reach of sense-and-thought. It is through superstition men thus impiously speak.\nFor all the things that are, Asclepius, all are in God, are brought by God to be, and do depend on Him - both things that act through bodies, and things that through soul-substance make [other things] to move, and things that make things live by means of spirit, and things that take unto themselves the things that are worn out.\nAnd rightly so; nay, I would rather say, He doth not have these things; but I speak forth the truth, He is them all Himself. He doth not get them from without, but gives them out [from Him].\nThis is God\'s sense-and-thought, ever to move all things. And never time shall be when e\'en a whit of things that are shall cease; and when I say "a whit of things that are", I mean a whit of God. For thigs that are, God hath; nor aught [is there] without Him, nor [is] He without aught.'
+
+				})
+		break;
+		
+		case 'CH9.10':
+			bot.sendMessage({
+
+                to: channelID,
+
+				message: '**CH9.10** *(G.R.S Mead)*\n>>> These things should seem to thee, Asclepius, if thou dost understand them, true; but if thou dost not understand, things not to be believed.\nTo understand is to believe, to not believe is not to understand.\nMy word (logos) doth go before [thee] to the truth. But mighty is the mind, and when it hath been led by word up to a certain point, it hath the power to come before [thee] to the truth.\nAnd having thought o\'er all these things, and found them consonant with those which have already been translated by the reason, it hath [e\'en now] believed, and found its rest in that Fair Faith.\nTo those, then, who by God[\'s good aid] do understand the things that have been said [by us] above, they\'re credible; but unto those who understand them not, incredible.\nLet so much, then, suffice on thought-and-sense.'				
+
+				})
+		break;
+				
+	}//CH9 end---
+	
+	
  }//else the message Poimandres heard didnt start with '?'...
  
 
