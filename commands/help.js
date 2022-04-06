57208c52d4579b1d78ca5e7b9814da6586ad1cf5
Barclay-McClay <102266976+Barclay-McClay@users.noreply.github.com>
2022-04-06 18:19:23 +1000

    github cleanup

--- commands/help.js
+++ commands/help.js
@@ -25,8 +25,13 @@ module.exports = {
     { name: '\u200B', value: '**Other Religious/Occult/Esoteric Texts:**'},
     { name: '`/quran`', value: "The Qur'an\nfrom `1.1` to `114.6`\nTranslated by Sahih International", inline: true },
     { name: '`/bible`', value: 'The Bible\nfrom `genesis 1.1` to `revelation 22.21`\nKing James Version', inline: true },
+    { name: '`/sepher-yetzirah`', value: 'Sepher Yetzirah\nfrom `1.1` to `6.4`\n*or* `/sepher-yetzirah path` from `1` to `32`\nTranslated by W.W. Wescott (1887)', inline: true },
     { name: '`/al`', value: 'Liber AL vel Legis or, *The Book of the Law*\nfrom `1.1` to `3.75`\nBook by Aiwass, Aleister Crowley, and Rose Edith Kelly (1904)', inline: true },
-    
+    { name: '`/enchiridion`', value: "Epictetus' Enchiridion\nfrom `1` to `51`\nTranslated by Thomas W. Higginson", inline: true },
+    { name: '`/aurelius`', value: "Marcus Aurelius' Meditations\nfrom `1.1` to `12.27`\nTranslated by Meric Casaubon", inline: true },
+
+    { name: '\u200B', value: '**Other tools:**'},
+    { name: '`/tarot', value: 'Rider-Waite-Smith Tarot Card pull\n`/tarot` for a random card\n`tarot 0-77` for a specific card'}
     )
 
     .setFooter({ text:'Poimandres Discord Bot • /help' })
