799d635aacf47d16a0347ca09fd6d3c854977c7a
Barclay-McClay <102266976+Barclay-McClay@users.noreply.github.com>
2022-04-22 11:25:07 +1000

    Added Proclus Elements of Metaphysics

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,9 @@ module.exports = {
     { name: '`/ch`', value: 'The Corpus Hermeticum\nfrom `1.1` to `18.16`\nTaken from *Thrice-Greatest Hermes Vol. 2*\nTranslated by G.R.S Mead (1906)', inline: true },
     { name: '`/ah`', value: 'Asclepius\nfrom `1` to `42`\nTaken from *Thrice-Greatest Hermes Vol. 2*\nTranslated by G.R.S Mead (1906)', inline: true  },
     { name: '`/emeraldtablet`', value: 'The Emerald Tablet\n*12th Century Latin version*\nTranslated by Steele and Singer (1928)', inline: true },
+    { name: '`/dh`', value: 'The Definitions\nHermes to Asclepius\nfrom `1.1` to `10.7`\nTranslated by Jean-Pierre Mahé (1999)', inline: true },
     { name: '`/oh`', value: 'The Oxford Fragments\nfrom `1` to `5`\nTranslated by Sartrix', inline: true },
+
 
     { name: '\u200B', value: '**Other Religious/Occult/Esoteric Texts:**'},
     { name: '`/quran`', value: "The Qur'an\nfrom `1.1` to `114.6`\nTranslated by Sahih International", inline: true },
@@ -30,6 +32,7 @@ module.exports = {
     { name: '`/al`', value: 'Liber AL vel Legis or, *The Book of the Law*\nfrom `1.1` to `3.75`\nBook by Aiwass, Aleister Crowley, and Rose Edith Kelly (1904)', inline: true },
     { name: '`/enchiridion`', value: "Epictetus' Enchiridion\nfrom `1` to `51`\nTranslated by Thomas W. Higginson", inline: true },
     { name: '`/aurelius`', value: "Marcus Aurelius' Meditations\nfrom `1.1` to `12.27`\nTranslated by Meric Casaubon", inline: true },
+    { name: '`/proclus-metaphysics`', value: "Proclus' Elements of Metaphysics\nfrom `1` to `211`\nTranslated by Antonio Vargas (2021)", inline: true },
 
     { name: '\u200B', value: '**Other tools:**'},
     { name: '`/tarot`', value: 'Rider-Waite-Smith Tarot Card pull\n`/tarot` for a random card\n`/tarot 0-77` for a specific card'}
